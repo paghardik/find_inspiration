0db462522d57dc0fff9789d966df1bc5ad612dc9
hardik <pag.hardik@gmail.com>
2021-03-21 12:31:44 +0530

    temp commit

--- lib/data/dummydata/story_model_data.dart
+++ lib/data/dummydata/story_model_data.dart
@@ -4,6 +4,18 @@ class StoryDummyData {
   @override
   Future<List<StoryModel>> call() async {
     return [
+      StoryModel("Shibuya Sushi", "18 CHF", "assets/pngs/img1.jpg"),
+      StoryModel("Super Salad", "12 CHF", "assets/pngs/img2.jpg"),
+      StoryModel("Angelo", "17 CHF", "assets/pngs/img3.jpg"),
+      StoryModel("Shibuya Sushi", "18 CHF", "assets/pngs/img1.jpg"),
+      StoryModel("Super Salad", "12 CHF", "assets/pngs/img2.jpg"),
+      StoryModel("Angelo", "17 CHF", "assets/pngs/img3.jpg"),
+      StoryModel("Shibuya Sushi", "18 CHF", "assets/pngs/img1.jpg"),
+      StoryModel("Super Salad", "12 CHF", "assets/pngs/img2.jpg"),
+      StoryModel("Angelo", "17 CHF", "assets/pngs/img3.jpg"),
+      StoryModel("Shibuya Sushi", "18 CHF", "assets/pngs/img1.jpg"),
+      StoryModel("Super Salad", "12 CHF", "assets/pngs/img2.jpg"),
+      StoryModel("Angelo", "17 CHF", "assets/pngs/img3.jpg"),
     ];
   }
 }
--- lib/data/repository/story_repository_impli.dart
+++ lib/data/repository/story_repository_impli.dart
@@ -1,17 +1,19 @@
+import 'package:find_inspiration/data/datasources/story_local_data_source.dart';
 import 'package:find_inspiration/domain/entities/story_entity.dart';
 import 'package:find_inspiration/domain/repository/story_repository.dart';
 
 class StoryRepositoryImpl extends StoryRepository{
+  final StoryLocalDataSource localDataSource;
+  StoryRepositoryImpl(this.localDataSource);
+
   @override
   Future<List<StoryEntity>> storyList() {
-    // TODO: implement storyList
-    throw UnimplementedError();
+    return localDataSource.storyList();
   }
 
   @override
   Stream<List<StoryEntity>> storyListStream() {
-    // TODO: implement storyListStream
-    throw UnimplementedError();
+    return localDataSource.storyListStream();
   }
 
 }
