5e0f517b09656c603e4c623dda272454d37ab448
hardik <pag.hardik@gmail.com>
2021-03-24 02:18:32 +0530

    update  translation_constants

--- lib/comman/translation_constants.dart
+++ lib/comman/translation_constants.dart
@@ -5,6 +5,7 @@ class TranslationConstants {
   static const String TITLE = 'Title';
   static const String POST_IN_STORY = 'Post in Story';
   static const String CREATE_STORY = 'Create Story';
+  static const String FIND_INSPIRATION = 'Find Inspiration';
 
   /*Error Message*/
   static const String PRICE_ERROR = 'Invalid Price';
