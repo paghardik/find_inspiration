3bbab990e039941b68bfa4e0d0a277b5d619d810
hardik <pag.hardik@gmail.com>
2021-03-24 02:21:52 +0530

    update text_themes

--- lib/presentation/themes/text_themes.dart
+++ lib/presentation/themes/text_themes.dart
@@ -15,43 +15,43 @@ class TextThemes {
   * */
 
   //Headline1 => Size => 96
-  static TextStyle get _tStyleHeadline1 => _textThemes.headline1.copyWith();
+  static TextStyle get _tStyleHeadline1 => _textThemes.headline1!.copyWith();
 
   //Headline2 => Size => 60
-  static TextStyle get _tStyleHeadline2 => _textThemes.headline2.copyWith();
+  static TextStyle get _tStyleHeadline2 => _textThemes.headline2!.copyWith();
 
   //Headline3 => Size => 48
-  static TextStyle get _tStyleHeadline3 => _textThemes.headline3.copyWith();
+  static TextStyle get _tStyleHeadline3 => _textThemes.headline3!.copyWith();
 
   //Headline4 => Size => 34
-  static TextStyle get _tStyleHeadline4 => _textThemes.headline4.copyWith();
+  static TextStyle get _tStyleHeadline4 => _textThemes.headline4!.copyWith();
 
   //Headline5 => Size => 24
-  static TextStyle get _tStyleHeadline5 => _textThemes.headline5.copyWith();
+  static TextStyle get _tStyleHeadline5 => _textThemes.headline5!.copyWith();
 
   //Headline6 => Size => 20
-  static TextStyle get _tStyleHeadline6 => _textThemes.headline6.copyWith();
+  static TextStyle get _tStyleHeadline6 => _textThemes.headline6!.copyWith();
 
   //SubTitle1 => Size => 16
-  static TextStyle get _tStyleSubTitle1 => _textThemes.subtitle1.copyWith();
+  static TextStyle get _tStyleSubTitle1 => _textThemes.subtitle1!.copyWith();
 
   //SubTitle2 => Size => 14
-  static TextStyle get _tStyleSubTitle2 => _textThemes.subtitle2.copyWith();
+  static TextStyle get _tStyleSubTitle2 => _textThemes.subtitle2!.copyWith();
 
   //body1 => Size => 16
-  static TextStyle get _tStyleBody1 => _textThemes.bodyText1.copyWith();
+  static TextStyle get _tStyleBody1 => _textThemes.bodyText1!.copyWith();
 
   //body2 => Size => 14
-  static TextStyle get _tStyleBody2 => _textThemes.bodyText2.copyWith();
+  static TextStyle get _tStyleBody2 => _textThemes.bodyText2!.copyWith();
 
   //button => Size => 14
-  static TextStyle get _tStyleButton => _textThemes.button.copyWith();
+  static TextStyle get _tStyleButton => _textThemes.button!.copyWith();
 
   //caption => Size => 12
-  static TextStyle get _tStyleCaption => _textThemes.caption.copyWith();
+  static TextStyle get _tStyleCaption => _textThemes.caption!.copyWith();
 
   //overline => Size => 10
-  static TextStyle get _tStyleOverLine => _textThemes.overline.copyWith();
+  static TextStyle get _tStyleOverLine => _textThemes.overline!.copyWith();
 
   static TextTheme get textTheme => TextTheme(
         headline1: _tStyleHeadline1,
