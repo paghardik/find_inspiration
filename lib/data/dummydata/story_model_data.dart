a0897361ef31b34334edc445c7d0ede98bc23752
hardik <pag.hardik@gmail.com>
2021-03-24 16:07:46 +0530

    update dummy data

--- lib/data/dummydata/story_model_data.dart
+++ lib/data/dummydata/story_model_data.dart
@@ -4,7 +4,7 @@ class StoryDummyData {
   @override
   Future<List<StoryModel>> call() async {
     return [
-      StoryModel(title: "Shibuya Sushi", price:"18 CHF",  imageUrl: "assets/pngs/img1.jpg"),
+   /*   StoryModel(title: "Shibuya Sushi", price:"18 CHF",  imageUrl: "assets/pngs/img1.jpg"),
       StoryModel(title:"Shibuya Sushi", price:"18 CHF", imageUrl:"assets/pngs/img1.jpg"),
       StoryModel(title:"Super Salad", price:"12 CHF", imageUrl:"assets/pngs/img2.jpg"),
       StoryModel(title:"Angelo", price:"17 CHF", imageUrl:"assets/pngs/img3.jpg"),
@@ -16,7 +16,7 @@ class StoryDummyData {
       StoryModel(title:"Angelo", price:"17 CHF", imageUrl:"assets/pngs/img3.jpg"),
       StoryModel(title:"Shibuya Sushi", price:"18 CHF", imageUrl:"assets/pngs/img1.jpg"),
       StoryModel(title:"Super Salad", price:"12 CHF", imageUrl:"assets/pngs/img2.jpg"),
-      StoryModel(title:"Angelo", price:"17 CHF", imageUrl:"assets/pngs/img3.jpg"),
+      StoryModel(title:"Angelo", price:"17 CHF", imageUrl:"assets/pngs/img3.jpg"),*/
     ];
   }
 }
